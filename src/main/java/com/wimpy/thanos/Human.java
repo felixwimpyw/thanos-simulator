f92ea4893f9dd80517994505d29b12c9b94c25c4
Felix Wimpy Wijaya <felixwimpy@gmail.com>
2019-06-12 14:07:09 +0700

    human bean

---
+++ src/main/java/com/wimpy/thanos/Human.java
@@ -0,0 +1,20 @@
+package com.wimpy.thanos;
+
+public class Human {
+
+  private int pilihan;
+
+  public Human(int pilihan) {
+    super();
+    this.pilihan = pilihan;
+  }
+
+  public int getPilihan() {
+    return pilihan;
+  }
+
+  public void setPilihan(int pilihan) {
+    this.pilihan = pilihan;
+  }
+  
+}
